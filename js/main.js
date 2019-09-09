e63f340703c4a80a7afe559d9d3fc441e1ddbefd
Kiiizuna <936511537@qq.com>
2019-09-09 23:38:33 +0800

    update update function

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 (function() {
+    'use strict';
+
+    var copy = function(obj) {
+        return Object.assign({}, obj)
+    }
     new Vue({
         el: '#main',
         data: {
@@ -6,13 +11,26 @@
             current: {},
         },
         methods: {
-            add: function() {
-                var title = this.current.title
-                if (!title || title === 0) {
-                    return
+            merge: function() {
+                var is_update = this.current.id
+                if (is_update) {
+                    var index = this.list.findIndex(function(item) {
+                        return item.id = is_update
+                    })
+                    Vue.set(this.list, index, copy(this.current))
+                    // this.list[index] = copy(this.current)
+                    log('list', this.list)
+                } else {
+                    var title = this.current.title
+                    if (!title || title === 0) {
+                        return
+                    }
+                    var todo = copy(this.current)
+                    todo.id = this.next_id()
+                    this.list.push(todo)                   
                 }
-                var todo = Object.assign({}, this.current)
-                this.list.push(todo)
+                this.reset_current() 
+
                 log('this.list:', this.list)
             },
             update: function() {
@@ -21,6 +39,19 @@
             remove: function(id) {
                 this.list.splice(id, 1)
             },
+            next_id: function() {
+                return this.list.length + 1
+            },
+            set_current: function(todo) {
+               this.current = copy(todo)
+            },
+            reset_current: function() {
+                this.set_current({})
+            }
         }
     })
 })()
+
+
+
+
